77090547a74c52f00bb8d9fcc8e252877f4ef350
muhammadaskar <mhmmdaskar11@gmail.com>
2022-07-03 00:17:16 +0700

    edit addParent

--- vue/src/views/Admin/Parent-Account/AddParent.vue
+++ vue/src/views/Admin/Parent-Account/AddParent.vue
@@ -32,6 +32,16 @@
           @submit.prevent="saveParent"
           enctype="multipart/form-data"
         >
+          <Alert
+            v-if="Object.keys(errors).length"
+            class="flex-col items-stretch text-sm"
+          >
+            <div v-for="(field, i) of Object.keys(errors)" :key="i">
+              <div v-for="(error, ind) of errors[field] || []" :key="ind">
+                * {{ error }}
+              </div>
+            </div>
+          </Alert>
           <div class="shadow sm:rounded-md sm:overflow-hidden">
             <!-- Teacher Account Fields -->
             <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
@@ -317,6 +327,27 @@
                   focus:ring-indigo-500
                 "
               >
+                <svg
+                  v-if="loading"
+                  class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    class="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    stroke-width="4"
+                  ></circle>
+                  <path
+                    class="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
                 Simpan
               </button>
             </div>
@@ -332,10 +363,13 @@ import { computed, ref, watch } from "vue";
 import PageComponent from "../../../components/PageComponent.vue";
 import { useRoute, useRouter } from "vue-router";
 import store from "../../../store";
+import Alert from "../../../components/Alert.vue";
 
 const router = useRouter();
 
 const route = useRoute();
+
+let errors = ref("");
 
 const students = computed(() => store.state.students);
 
@@ -369,17 +403,43 @@ function onImageChoose(e) {
   reader.readAsDataURL(file);
 }
 
+let loading = ref(false);
+
 function saveParent() {
-  console.log(model.student_id);
-  store.dispatch("saveParentAccount", model.value).then(({ data }) => {
+  loading.value = true;
+  if (
+    model.value.name == "" ||
+    model.value.email == "" ||
+    model.value.nik == "" ||
+    model.value.gender == "" ||
+    model.value.address == ""
+  ) {
+    loading.value = false;
     store.commit("notify", {
-      type: "success",
-      message: "akun orang tua berhasil disimpan ",
+      type: "failed",
+      message: "form wajib diisi",
     });
-    router.push({
-      name: "AdminParentAccount",
+  } else {
+    console.log(model.student_id);
+    store.dispatch("saveParentAccount", model.value).then(({ data }) => {
+      loading.value = false;
+      store.commit("notify", {
+        type: "success",
+        message: "akun orang tua berhasil disimpan ",
+      });
+      router
+        .push({
+          name: "AdminParentAccount",
+        })
+        .catch((error) => {
+          loading.value = false;
+          console.error(error.response.status);
+          if (error.response.status === 422) {
+            errors.value = error.response.data.errors;
+          }
+        });
     });
-  });
+  }
 }
 </script>
 
